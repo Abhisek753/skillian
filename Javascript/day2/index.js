632c8c11c5057e30001693601ea1af3874b3eeaa
Abhisek753 <properachievement@gmail.com>
2025-02-23 16:28:53 +0530

    add js loops

---
+++ Javascript/day2/index.js
@@ -0,0 +1,25 @@
+// console.log("Hello world")
+// console.log("10"+20+30)
+
+
+let arr=[2,3,5,7,9]
+//0/1/2/3/4
+// for(let i=1;i<=4;i+=2){
+//     console.log("Hello World");
+// }
+// for(let i=0;i<=arr.length-1;i++){
+//     console.log(arr[i])
+// }
+
+//while loop
+// let i=1;
+// while(i<=5){
+//     console.log(i)
+//     i++
+// }
+
+let j=10;
+do{
+    console.log(j)
+    j++
+}while(j<=11);
