abfb82b9fd081d31a20af000df6980952c869787
Abhisek753 <properachievement@gmail.com>
2025-02-23 14:39:34 +0530

    add

---
+++ Javascript/day1/index.js
@@ -0,0 +1,74 @@
+// console.log("hello Abhisek");
+// var name="Swati";
+// console.log(name);
+// let name2="Swati2";
+// console.log(name2);
+// const name3="Swati3";
+// console.log(name3);
+// var name ="Swati"
+// console.log(typeof name);
+// var number1=200.54;
+// console.log(typeof(number1));
+// let age;
+// console.log(age);
+// let val=Symbol("name");
+// console.log(typeof val);
+//  let obj={name:"Abhisek",age:25}
+//  console.log(typeof obj)
+// let arr=["mango","grapes"];
+// let arr=["yellow","green","teal",4,7,9,90];
+// // 0,1,2
+// // console.log(col[6])
+// console.log(Array.isArray(arr));
+
+//Mathematical operator
+// var a=5;
+// var b=3;
+// console.log(a*b);
+// var a=15;
+// var b=3;
+// console.log(a/b);
+
+//Modulo
+
+// let a=2;
+// let b=4;
+// // console.log(a/b);
+// // console.log(a*b)   
+// console.log(a**b)  
+
+// let str1="name";
+// let str2="Abhisek"
+// let str3="Fahim"
+// console.log(str1,"\n",str2)
+// console.log(`${str1} 
+// ${str2}  ${str3}`)
+
+
+// var num1=1;
+// var num2=2;
+// var output="1 + 2 =" + typeof(num1+num2)
+// console.log(output)
+// console.log(typeof output)
+
+//Relational operator
+// let a=5;
+// let b=5;
+// console.log(a<b);
+//Conditional operator
+
+// if("name"=="name"){
+//     console.log("It is true")
+//     console.log(3+7);
+// }else if(4>13){
+//     console.log("2nd is passing")
+// }
+// else{
+//     console.log("It is false")
+
+// }
+
+// (10<5)?console.log("print true"):console.log("print false");
+let a="4";
+let b=10;
+console.log(Number(a))
