c2c6c037caceb17dd5b1a60b66e3b9dfac141d4f
Abhisek753 <properachievement@gmail.com>
2025-03-02 23:41:08 +0530

    added

---
+++ Javascript/day3/index.js
@@ -0,0 +1,187 @@
+// console.log("hello world");
+
+// function printName(name1,name2){
+//     console.log("two names",name1,name2);
+// }
+// printName("swati","Amaan")
+
+// function Add(val1,val2){
+//     console.log(val1+Number(val2))
+// }
+// Add(2,"3")
+
+// function Sub({name}){
+// return name
+// }
+// let result=Sub({name:"kumal"});
+// console.log(result)
+// function Print(){
+//     var name="Kunal"
+//     console.log(name)
+// }
+// Print()
+// console.log("name:",name)
+
+// var fruit="Apple"
+//  let ans=fruit.toUpperCase()
+//  let ans2=fruit.toLowerCase()
+//  console.log(ans);
+//  console.log(ans2)
+
+// let val="   This name is fahim    ";
+//    // 15
+// // let ans=val.substring(0,15);
+// let ans2=val.slice(-3,-1)
+// console.log(ans2);
+// let name=val.indexOf("is",4)
+// let name=val.lastIndexOf("is")
+
+// console.log(name)
+// let ans=val.replace("This","Hello");
+// console.log(ans)
+
+// console.log(val)
+// val.trim()
+// console.log(val)
+
+// let data="apple,orange,pineapple"
+// let ans=data.split(",")
+// for(let val of ans){
+//     console.log(val)
+// }
+
+// let arr=[1,2,3,4]
+// arr.push(5)
+// console.log(arr)
+// //[1,2,3,4,5]
+// arr.pop()
+// console.log(arr)
+// arr.unshift(9);
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+ 
+// let arr=[1,2,3,6,8,9];
+// let ans=arr.slice(2,4)
+// console.log(ans)
+
+// let arr1=[5,6];
+// let arr2=[1]
+// let ans=arr1.concat(arr2)
+// console.log(ans)
+
+// let arr=["Abhisek","Fahim","Kunal"];
+// let ans=arr.join(",")
+// console.log(ans)
+
+//modification array
+// let arr=[1,2,3,4];
+// // arr [{},{}]
+// arr.forEach((el,i,arr)=>{
+//     arr[i]=el*2
+// })
+// console.log(arr)
+
+// let ans=arr.map((el)=>{
+//     return el*2
+// })
+// console.log(arr,"orig");
+
+// console.log(ans);
+// let sum=0
+// const numbers = [65, 44, 12, 4];
+// numbers.forEach(myFunction);
+
+// function myFunction(item) {
+//   sum += item;
+// }
+
+
+// let arr=[1,2,3];
+// // let ans= arr.map((el,i,arr)=>{
+// //     return arr[i]>4
+// // })
+// // console.log(arr,"orig")
+
+// // console.log(ans)
+
+// let sum=arr.reduce((acc,curr)=>{
+//     return acc+curr
+// });
+// console.log(sum);
+
+// let student=[
+//     {name:"raj",age:2},
+//     {name:"raj2",age:4},
+//     {name:"raj3",age:3},
+
+// ]
+// student=[0,4,5]
+// let totlaAge=student.reduce((sum,student)=>{
+//     console.log("acc",sum)
+//     return sum*student
+// },0)
+// console.log(totlaAge);
+
+// let val123="arjun"
+// console.log(val123);
+// console.log(this)
+
+// console.log(num)
+
+// let num
+
+
+
+// console.log(fun)
+//  var fun=function(){ 
+    
+//     var num=9
+//     console.log("hello",num)
+    
+// }
+
+// Print()
+// if(true){
+//    var num=7;
+// }
+// console.log("hello",num);
+// {
+//    // let const
+// }
+
+
+
+// let name1="ram"
+// name1="shyam";
+// console.log(name1)
+
+// function test() {
+//     var a = 10;
+//     console.log(a); // ✅ 10
+//   }
+//   console.log(a); // ❌ Error
+  
+  // if (true) {
+  //   var b = 20;
+  //   console.log(b); // ✅ 20
+  // }
+  // console.log(b);
+// console.log(myName)
+//   function myName(){
+//     console.log("hello abhisek")
+//   }
+//   myName()
+//   console.log(myName);
+
+// var testFunc=function(){
+//   console.log("test func")
+// }
+// testFunc()
+// console.log(fun)
+// fun()
+// var fun=function Myfunc(){
+//   console.log("hellow world")
+// };
+// fun()
+
